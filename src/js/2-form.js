cb561640cfd497b59704ad9d90c43eff27da3464
IULIIA VOSKOBOINYK <juliia.whatever@gmail.com>
2024-12-12 23:49:40 +0100

    4

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,23 +6,23 @@ const formData = {
 };
 
 const fillFormField = () => {
-    try {
-        const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
-        if (formDataFromLS === null) {
-            return;
-        }
-
-        const formDataFromLSKeys = Object.keys(formDataFromLS);
-
-        formDataFromLSKeys.forEach(key => {
-            feedbackFormEl.elements[key].value = formDataFromLS[key];
-            formData[key] = formDataFromLS[key];
-        });
-
-        console.log(formData);
-    } catch (err) {
-        console.log(err);
+  try {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (formDataFromLS === null) {
+      return;
     }
+
+    const formDataFromLSKeys = Object.keys(formDataFromLS);
+
+    formDataFromLSKeys.forEach(key => {
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
+      formData[key] = formDataFromLS[key];
+    });
+
+    console.log('Initial formData:', formData);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fillFormField();
@@ -34,12 +34,25 @@ const onFormFieldInput = event => {
   const fieldValue = formField.value;
 
   formData[fieldName] = fieldValue;
-    
+
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const updateFormData = () => {
+  const formElements = feedbackFormEl.elements;
+  Object.keys(formData).forEach(key => {
+    if (formElements[key]) {
+      formData[key] = formElements[key].value;
+    }
+  });
 };
 
 const onFeedbackFormSubmit = event => {
   event.preventDefault();
+
+  updateFormData();
+
+  console.log('Before submit:', formData);
 
   const formDataValues = Object.values(formData);
 
@@ -48,6 +61,14 @@ const onFeedbackFormSubmit = event => {
 
     return;
   }
+
+  console.log('Submitted form data:', formData);
+
+  Object.keys(formData).forEach(key => {
+    formData[key] = '';
+  });
+
+  console.log('After clear:', formData);
 
   event.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
